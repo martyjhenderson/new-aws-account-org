9054ad2a564e931e3cc1b8b7ba5ee33093d0b171
Marty Henderson <marty@ravegrunt.is>
2024-07-28 13:53:26 -0500

    Clarifying variables

--- terraform/new_account.tf
+++ terraform/new_account.tf
@@ -1,6 +1,6 @@
 resource "aws_organizations_account" "account" {
   name              = var.new_account_name
   email             = var.account_email
-  close_on_deletion = true
+  close_on_deletion = var.delete_account
   parent_id         = var.parent_ou
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,9 @@ variable "parent_ou" {
   type        = string
   description = "ID of the OU you want the account under"
 }
+
+variable "delete_account" {
+  type        = string
+  description = "Should the account be deleted when destroy is ran?"
+  default     = "false"
+}
